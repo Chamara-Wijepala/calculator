e5eae4a4e077590095dadacfd1e48310ea84672e
Chamara-Wijepala <mr.worf777@gmail.com>
2021-03-18 15:11:44 +0530

    Create functions to add button values to display

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-function add(x, y) {console.log(x + y)};
+let displayValue = "";
+
+function setDisplay(displayValue) {
+    document.getElementById('display').innerText = displayValue;
+};
+
+function buttonClick(e) {
+    let button = e.target.innerText.toString();
+    displayValue = displayValue + " " + button;
+    setDisplay(displayValue);
+};
+
+document.querySelectorAll('button').forEach(button => {button.addEventListener('click', buttonClick)});
+
+function add(x, y) {return x + y};
 function subtract(x, y) {return x - y};
 function multiply(x, y) {return x * y};
 function divide(x, y) {return x / y};

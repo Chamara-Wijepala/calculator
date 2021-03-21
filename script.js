a7ca8ed82621ffc6fe521ebf37bb5f3768474260
Chamara-Wijepala <mr.worf777@gmail.com>
2021-03-21 16:46:50 +0530

    Add basic arithmetic functionality

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-let displayValue = "0";
+let displayValue = "";
 let operator = "";
 let num1 = "";
 let num2 = "";
+let currentValue = "";
 
 function setDisplay(displayValue) {
     document.getElementById('display').innerText = displayValue;
 };
 
 function clearDisplay() {
-    displayValue = "0";
+    displayValue = "";
     setDisplay(displayValue);
 };
 
-document.querySelector('#clear').addEventListener('click', clearDisplay);
+function clear() {
+    operator = "";
+    num1 = "";
+    num2 = "";
+    currentValue = "";
+};
+
+document.querySelector('#clear').addEventListener('click', clear);
 
 function buttonClick(e) {
     let button = e.target.innerText.toString();
@@ -20,40 +28,50 @@ function buttonClick(e) {
     switch(button) {
         default:
             displayValue += button;
+            currentValue += button;
             setDisplay(displayValue);
             break;
         case "÷":
         case "×":
         case "+":
+        case "-":
+            num1 = currentValue;
+            operator = button;
             displayValue += " " + button + " ";
+            currentValue = "";
             setDisplay(displayValue);
             break;
-        case "-":
         case "=":
+            num2 = currentValue;
+            operate(operator, num1, num2);
+            clear();
             break;
     };
 };
 
 document.querySelectorAll('.key').forEach(button => {button.addEventListener('click', buttonClick)});
 
-function add(x, y) {return x + y};
-function subtract(x, y) {return x - y};
-function multiply(x, y) {return x * y};
-function divide(x, y) {return x / y};
+function add(x, y) {console.log(x + y)};
+function subtract(x, y) {console.log(x - y)};
+function multiply(x, y) {console.log(x * y)};
+function divide(x, y) {console.log(x / y)};
 
 function operate(operator, num1, num2) {
+    let value1 = Number(num1);
+    let value2 = Number(num2);
+
     switch(operator) {
-        case "add":
-            add(num1, num2);
+        case "+":
+            add(value1, value2);
             break;
-        case "subtract":
-            subtract(num1, num2);
+        case "-":
+            subtract(value1, value2);
             break;
-        case "multiply":
-            multiply(num1, num2);
+        case "×":
+            multiply(value1, value2);
             break;
-        case "divide":
-            divide(num1, num2);
+        case "÷":
+            divide(value1, value2);
             break;
     };
 };
